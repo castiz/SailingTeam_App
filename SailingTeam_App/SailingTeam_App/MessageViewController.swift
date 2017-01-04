ab63ca57fdcb747aff3912ae1fc02d128c0d608b
Casey Astiz <castiz@middlebury.edu>
2017-01-03 18:32:06 -0800

    Add files via upload

---
+++ SailingTeam_App/SailingTeam_App/MessageViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MessageViewController.swift
+//  SailingTeam_App
+//
+//  Created by Casey Astiz on 1/2/17.
+//  Copyright © 2017 Casey Astiz. All rights reserved.
+//
+
+import UIKit
+
+class MessageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SailingTeam_App/SailingTeam_App/ScheduleViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ScheduleViewController.swift
+//  SailingTeam_App
+//
+//  Created by Casey Astiz on 1/2/17.
+//  Copyright © 2017 Casey Astiz. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
